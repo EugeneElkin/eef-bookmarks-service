774c14d68e4f0b562f794ed4e7d6b3e5904b211b
Evgenii Elkin <eef84@mail.com>
2018-11-10 00:59:03 +0300

    IIS Express configuration preparation script was implemented

--- tools/api-prepare-config.js
+++ tools/api-prepare-config.js
@@ -1,3 +1,34 @@
 "use strict";
 
-console.log("TODO: There must be a script that prepare published api config to deploy API on IISEXpress host");
+const fs = require('fs');
+const appRoot = require('app-root-path');
+
+const apiPhysicalPath = appRoot.path + "\\dist_api";
+const tmpDir = appRoot.path + "\\.tmp";
+const outputConfigFilePath = tmpDir + "\\apihost.config";
+
+fs.readFile("./tools/apihost.config.template", 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    const result = data.replace("[PUBLISHED_API_ABSOLUTE_PATH]", apiPhysicalPath);
+
+    
+    if (!fs.existsSync(tmpDir)) {
+        fs.mkdir(tmpDir, { recursive: true }, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
+
+    fs.writeFile(outputConfigFilePath, result, 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});
+
+console.log("The location of IISExpress configuration file is [" + outputConfigFilePath + "]");
+console.log("The WebAPI host path was set as [" + apiPhysicalPath + "]");
